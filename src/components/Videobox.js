f93b61d0fdb51761f7371b1b74966cdc88e6a3e1
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2016-12-12 15:07:45 +0100

    deployed on gh pages

---
+++ src/components/Videobox.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './../css/Videobox.css';
+
+class Videobox extends Component {
+  render() {
+    return (
+      <iframe
+          src={this.props.url}
+          height="500"
+          width="900"
+          frameBorder="0"
+          scrolling="no"
+          allowFullScreen="true">
+      </iframe>
+    );
+  }
+}
+
+export default Videobox;
